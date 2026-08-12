9695a2e8016e6d336ae1cead6bc00a6e790f097c
SurajKH57 <surajkaarthi.23@gmail.com>
2026-08-12 17:03:09 +0530

    created .gitignore

---
+++ Addition.X/add.asm
@@ -0,0 +1,9 @@
+#include<p18f452.inc>
+    ORG 00H
+    
+    MOVLW d'15' ; d is to denote us handling decimal values.
+    ADDLW d'20'
+    END
+
+
+
---
+++ Addition.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Addition.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Addition.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=addition.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/addition.x.tar
